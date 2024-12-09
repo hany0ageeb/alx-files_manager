c14721e05fbbbf3cfedfc05fe372984914ddd5a5
hany0ageeb <hany0ageeb@gmail.com>
2024-12-09 18:20:00 +0200

    fixing eslint

--- utils/redis.js
+++ utils/redis.js
@@ -6,7 +6,7 @@ class RedisClient {
     this._client = createClient();
     this._alive = true;
     this._client.on('error', (err) => {
-      console.log('Redis connection error:', err);
+      console.log(`Redis connection error: ${err}`);
       this._alive = false;
     });
   }
@@ -18,19 +18,16 @@ class RedisClient {
   async get(key) {
     const GET = promisify(this._client.GET);
     return GET.call(this._client, key);
-    //return await this._client.get(key);
   }
 
   async set(key, value, expires) {
     const SETEX = promisify(this._client.SETEX);
     await SETEX.call(this._client, key, expires, value);
-    //await this._client(key, expires, value);
   }
 
   async del(key) {
     const DEL = promisify(this._client.DEL);
     return DEL.call(this._client, key);
-    //return await this._client.del(key);
   }
 }
 
