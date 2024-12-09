52cdeaf8e6cfb39fdd8474f80073a8edc60f77f4
hany0ageeb <hany0ageeb@gmail.com>
2024-12-09 19:41:45 +0200

    FilesController

---
+++ utils/transform.js
@@ -0,0 +1,20 @@
+const transform = (data) => {
+  const transformed = [];
+  for (const row of data) {
+    const refined = {};
+    refined.id = row._id.valueOf();
+    refined.userId = row.userId.valueOf();
+    refined.name = row.name;
+    refined.type = row.type;
+    refined.isPublic = row.isPublic;
+    if (row.parentId === '0') {
+      refined.parentId = '0';
+    } else {
+      refined.parentId = row.parentId.valueOf();
+    }
+    transformed.push(refined);
+  }
+  return transformed;
+};
+
+module.exports = transform;
