5eaf09170976f5381cc12f6df3605123fbf3f6b7
hany0ageeb <hany0ageeb@gmail.com>
2024-12-09 18:58:36 +0200

    adding UsersController in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/stats', AppController.getStats);
 router.get('/status', AppController.getStatus);
 
-router.post('/users', UserController.postNew);
-router.get('/users/me', UserController.getMe);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
