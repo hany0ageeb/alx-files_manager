f60775b030147ce8ffb5b63f27fcdda88929194c
hany0ageeb <hany0ageeb@gmail.com>
2024-12-09 18:37:49 +0200

    server.js

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+
+const router = Router();
+
+router.get('/stats', AppController.getStats);
+router.get('/status', AppController.getStatus);
+
+export default router;
