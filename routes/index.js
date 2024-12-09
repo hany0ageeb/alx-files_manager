5de67c758a85ec7ea315a5c425a08a203edb1cb3
hany0ageeb <hany0ageeb@gmail.com>
2024-12-09 20:04:57 +0200

    FileContoller.js

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,9 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
 router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
+router.get('/files/:id', FilesController.getShow);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 export default router;
